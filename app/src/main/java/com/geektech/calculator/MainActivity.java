98b45c912c0aa60c21cee980bfa2a7e02f4ffcb4
Uran667 <uranberdibekov11@gmail.com>
2022-03-28 00:03:32 +0600

    Initial commit

---
+++ app/src/main/java/com/geektech/calculator/MainActivity.java
@@ -0,0 +1,162 @@
+package com.geektech.calculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView tvResult;
+    private Integer first, second;
+    private Boolean isOperationClick;
+    private String oriation;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        tvResult = findViewById(R.id.tv_result);
+
+    }
+
+    public void onNumberClick(View view) {
+        switch (view.getId()){
+            case R.id.btn_one:
+
+                tvResult.setText("1");
+
+
+
+
+                break;
+            case R.id.btn_two:
+                tvResult.setText("2");
+
+
+                break;
+            case R.id.btn_three:
+                tvResult.setText("3");
+
+
+                break;
+            case R.id.btn_fhour:
+                tvResult.setText("4");
+
+
+                break;
+            case R.id.btn_five:
+            tvResult.setText("5");
+
+
+            break;
+            case R.id.btn_six:
+                tvResult.setText("6");
+
+
+                break;
+            case R.id.btn_seven:
+                tvResult.setText("7");
+
+
+                break;
+            case R.id.btn_eight:
+                tvResult.setText("8");
+
+
+                break;
+            case R.id.btn_nine:
+                tvResult.setText("9");
+
+
+                break;
+            case R.id.btn_zero:
+                tvResult.setText("0");
+
+
+                break;
+            case R.id.btn_tochka:
+                tvResult.setText(".");
+
+
+                break;
+            case R.id.btn_clear:
+                tvResult.setText("0");
+                first = 0;
+                second = 0;
+                break;
+        }
+    }
+
+    public void onOperationClick(View view) {
+        switch (view.getId()){
+            case R.id.btn_plus:
+                    oriation = "+";
+
+                first = Integer.parseInt(tvResult.getText().toString());
+                isOperationClick = true;
+                break;
+            case R.id.btn_minus:
+                oriation = "-";
+
+                first = Integer.parseInt(tvResult.getText().toString());
+                isOperationClick = true;
+                break;
+            case R.id.btn_multiplacation:
+                oriation = "x";
+
+                first = Integer.parseInt(tvResult.getText().toString());
+                isOperationClick = true;
+                break;
+            case R.id.btn_division:
+                oriation = "/";
+
+                first = Integer.parseInt(tvResult.getText().toString());
+                isOperationClick = true;
+                break;
+            case R.id.btn_precent:
+                oriation = "%";
+
+                first = Integer.parseInt(tvResult.getText().toString());
+                isOperationClick = true;
+                break;
+            case R.id.btn_plusminus:
+                oriation = "+/-";
+
+                first = Integer.parseInt(tvResult.getText().toString());
+                isOperationClick = true;
+                break;
+
+
+            case R.id.btn_equal:
+                second = Integer.parseInt(tvResult.getText().toString());
+                Integer result = 0;
+
+                isOperationClick = true;
+                switch (oriation){
+                    case "+":
+                result  = first + second;
+                break;
+                    case "+/-":
+                        result = first - (first * 2);
+                        result = second - (second * 2);
+                    case "-":
+                        result  = first - second;
+                        break;
+                    case "x":
+                        result  = first * second;
+                        break;
+                    case "/":
+                        result  = first / second;
+                        break;
+                    case "%":
+                        result  = (second * 100) / first;
+                        break;
+                }
+                tvResult.setText(result.toString());
+                break;
+        }
+
+    }
+}
